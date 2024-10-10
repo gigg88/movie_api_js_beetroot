b7cd862314c8884d6a66958171b72f3d7b8990f6
gigg88 <popp.christian88@gmail.com>
2024-10-10 21:01:29 +0300

    x

--- dist/movieapi.dev.js
+++ dist/movieapi.dev.js
@@ -5,7 +5,7 @@ var movieDataList = document.getElementById("movieDataList");
 var movieContainer = document.getElementById("movie_container");
 var movieBox = document.getElementById("movie_box");
 var title = document.getElementById("title");
-movieDataList.addEventListener("change", function () {
+movieDataList.addEventListener("click", function () {
   var movie = movieDataList.value;
 
   if (movie) {
@@ -17,15 +17,12 @@ movieDataList.addEventListener("change", function () {
       var moviePoster = data.Poster;
       var release_date = data.Year;
       var overview = data.Plot;
-      movieBox.innerHTML = "\n                <h2 id=\"title\">".concat(movieTitle, "</h2>\n                <p id=\"release-date\">").concat(release_date, "</p>\n                <p id=\"overview\">").concat(overview, "</p>  \n                <img src=\"").concat(moviePoster, "\" alt=\"\">\n                  \n            ");
+      movieBox.innerHTML = "\n            <h2 id=\"title\">".concat(movieTitle, "</h2>\n            <p id=\"release-date\">").concat(release_date, "</p>\n            <p id=\"overview\">").concat(overview, "</p>  \n            <img src=\"").concat(moviePoster, "\" alt=\"\">\n          ");
       movieBox.classList.add('active');
     })["catch"](function (error) {
       movieBox.innerHTML = "<p>Error: ".concat(error.message, "</p>");
     });
-  }
-
-  if (movie.value == '') {
+  } else {
     movieBox.classList.remove('active');
-    movieBox.innerHTML = "\n        <h2 id=\"title\">".concat(movieTitle, "</h2>\n        <p id=\"release-date\">").concat(release_date, "</p>\n        <p id=\"overview\">").concat(overview, "</p>  \n        <img src=\"").concat(moviePoster, "\" alt=\"\">\n          \n    ");
   }
 });
--- movieapi.js
+++ movieapi.js
@@ -5,46 +5,36 @@ const movieBox = document.getElementById("movie_box");
 const title = document.getElementById("title");
 
 
-movieDataList.addEventListener("change", () => {
+movieDataList.addEventListener("click", () => {
     const movie = movieDataList.value;
-
-    if(movie) {
-        const apiUrl = `https://www.omdbapi.com/?t=${movie}&apikey=${APIKEY}`;
-
-        fetch(apiUrl)
+  
+    if (movie) {
+      const apiUrl = `https://www.omdbapi.com/?t=${movie}&apikey=${APIKEY}`;
+  
+      fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-
-            const movieTitle = data.Title;
-            const moviePoster = data.Poster;
-            const release_date = data.Year;
-            const overview = data.Plot;
-            
-            movieBox.innerHTML = `
-                <h2 id="title">${movieTitle}</h2>
-                <p id="release-date">${release_date}</p>
-                <p id="overview">${overview}</p>  
-                <img src="${moviePoster}" alt="">
-                  
-            `;
-            movieBox.classList.add('active');
+          const movieTitle = data.Title;
+          const moviePoster = data.Poster;
+          const release_date = data.Year;
+          const overview = data.Plot;
+  
+          movieBox.innerHTML = `
+            <h2 id="title">${movieTitle}</h2>
+            <p id="release-date">${release_date}</p>
+            <p id="overview">${overview}</p>  
+            <img src="${moviePoster}" alt="">
+          `;
+  
+          movieBox.classList.add('active');
         })
         .catch(error => {
-            movieBox.innerHTML = `<p>Error: ${error.message}</p>`;
+          movieBox.innerHTML = `<p>Error: ${error.message}</p>`;
         });
-        
-    } if(movie.value == '') {
-        movieBox.classList.remove('active');
-
-        movieBox.innerHTML = `
-        <h2 id="title">${movieTitle}</h2>
-        <p id="release-date">${release_date}</p>
-        <p id="overview">${overview}</p>  
-        <img src="${moviePoster}" alt="">
-          
-    `;
+    } else {
+      movieBox.classList.remove('active');
     }
-});
+  });
 
 
 
